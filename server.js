47b9d3201a77e9fdaef469c83e694ecc19fe01b8
Jon Willemain <jon.willemain@gmail.com>
2017-06-01 11:55:55 -0600

    Add catch if model id cannot be found

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.listen(app.get('port'), () => {
 app.get('/', (request, response) => {
   fs.readFile(`${__dirname}/index.html`, (err, file) => {
     response.send(file)
-  })
+  });
 })
 
 app.get('/api/v1/model', (request, response) => {
@@ -29,16 +29,20 @@ app.get('/api/v1/model', (request, response) => {
       response.status(200).json(model)
     })
     .catch(error => {
-      console.error('error: ', error)
+      cresponse.status(404).send('We cannot find the url you were looking for.');
     });
 })
 
 app.get('/api/v1/model/:id', (request, response) => {
   database('model').where('id', request.params.id).select()
     .then(model => {
-      response.status(200).json(model)
+      if(!model.length) {
+        response.status(404).send(`we cannot find the model you are looking for`);
+      } else {
+        response.status(200).json(model)
+      }
     })
     .catch(error => {
-      console.error('error: ', error)
+      response.status(500).send(`${error}`);
     });
 })
